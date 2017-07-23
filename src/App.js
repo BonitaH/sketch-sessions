76c88f76040fedbda8223e3b5c31f9bc491869e7
BonitaH <bonitaa@outlook.com>
2017-07-23 17:43:23 +0200

    Made a form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,221 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-class Text extends React.Component {
+class Header extends React.Component {
+   render() {
+      return (
+        <div>
+            <img src={'logo.png'} />
+            <h1>Learning to draw</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>What do you want to draw?</h2>
+            <p></p>
+         </div>
+      );
+   }
+}
+class SelectExpressions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'both'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Choose expression: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
-    return <p>hallo</p>
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Choose facial expression?:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="all">all</option>
+            <option value="neutral">neutral</option>
+            <option value="happy">happy</option>
+            <option value="sad">sad</option>
+            <option value="angry">angry</option>
+            <option value="goofy">goofy</option>
+            <option value="surprised">surprised</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+class SelectGender extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'both'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('You want to draw: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Do you want to draw men or women?:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="both">both</option>
+            <option value="men">men</option>
+            <option value="women">women</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
-class App extends React.Component {
+class SelectSessions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'both'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('What kind of session: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          What kind of session?:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="All the same length">All the same length</option>
+            <option value="Class mode">Class mode</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+var Session = React.createClass({
+
+  getInitialState: function () {
+    return {
+      selectedOption: 'option1',
+      text: 'hello'
+    };
+  },
+
+  handleOptionChange: function (changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  },
+
+  handleFormSubmit: function (formSubmitEvent) {
+    formSubmitEvent.preventDefault();
+
+    console.log('You have selected:', this.state.selectedOption);
+  },
+
+  handleTextChange: function(changeEvent) {
+    this.setState({
+      text: changeEvent.target.value
+    })
+  },
+
+  render: function () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+
+          <br />
+
+          {this.state.text}
+
+          <input type="text" value={this.state.text} onChange={this.handleTextChange} />
+
+          <br />
+
+            <form onSubmit={this.handleFormSubmit}>
+              <div className="radio">
+                <label>
+                  Do you want to draw men or women: <br/>
+                  <input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange} />
+                  Both
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange}/>
+                  Men
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleOptionChange}/>
+                  Women
+                </label>
+              </div>
+              <button className="btn btn-default" type="submit">Save</button>
+            </form>
+
+          </div>
         </div>
-        <img src="aneta-ivanova-776.jpg" />
-        <Text></Text>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
+});
+
+
+
+class App extends React.Component {
+   render() {
+      return (
+         <div className="centerDiv">
+            <Header/>
+            <Content/>
+            <SelectGender/>
+            <SelectExpressions/>
+            <SelectSessions/>
+            <Session />
+         </div>
+      );
+   }
 }
 
 export default App;
