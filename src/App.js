34f97e60b9edcb4a84d2d32009ce9469a79b9636
BonitaH <bonitaa@outlook.com>
2017-07-24 15:56:30 +0200

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import { Button } from 'react-bootstrap';
 
-class Header extends React.Component {
+
+class Logo extends React.Component {
    render() {
       return (
         <div>
-            <img src={'logo.png'} />
-            <h1>Learning to draw</h1>
+           <img src={'LOGOEXAMPLE.png'} />
+         </div>
+      );
+   }
+}
+class Header extends React.Component {
+   render() {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+            <h1 className="Header"> Learn to draw </h1>
+         </div>
+         </div>
          </div>
       );
    }
@@ -15,120 +31,96 @@ class Header extends React.Component {
 class Content extends React.Component {
    render() {
       return (
-         <div>
-            <h2>What do you want to draw?</h2>
-            <p></p>
+        <div className="Content container">
+          <div className="row">
+            <div className="col-sm-12">
+            <h2 className="Content">What do you want to draw?</h2>
+         </div>
+         </div>
          </div>
       );
    }
 }
-class SelectExpressions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'both'};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+var Expressions = React.createClass({
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+  getInitialState: function () {
+    return {
+      selectedOption: 'option1',
+      text: 'hello'
+    };
+  },
 
-  handleSubmit(event) {
-    alert('Choose expression: ' + this.state.value);
-    event.preventDefault();
-  }
+  handleOptionChange: function (changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  },
 
-  render() {
+  handleFormSubmit: function (formSubmitEvent) {
+    formSubmitEvent.preventDefault();
+
+    console.log('You have selected:', this.state.selectedOption);
+  },
+
+  handleTextChange: function(changeEvent) {
+    this.setState({
+      text: changeEvent.target.value
+    })
+  },
+
+  render: function () {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Choose facial expression?:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="all">all</option>
-            <option value="neutral">neutral</option>
-            <option value="happy">happy</option>
-            <option value="sad">sad</option>
-            <option value="angry">angry</option>
-            <option value="goofy">goofy</option>
-            <option value="surprised">surprised</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+     <div className="Radio container">
+      <h3 className="radioLabel">Expression:</h3> 
+        <div className="row">
+          <div className="col-sm-12">
+            <form onSubmit={this.handleFormSubmit}>
+              <div className="radio">
+                <label className="biglabel">
+                  <input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange} />
+                  All
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange}/>
+                  Neutral
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleOptionChange}/>
+                  Happy
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option4" checked={this.state.selectedOption === 'option4'} onChange={this.handleOptionChange}/>
+                Sad
+                </label>
+              </div>
+              <div className="radio">
+                  <label>
+                    <input type="radio" value="option5" checked={this.state.selectedOption === 'option5'} onChange={this.handleOptionChange}/>
+                    Angry
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input type="radio" value="option6" checked={this.state.selectedOption === 'option6'} onChange={this.handleOptionChange}/>
+                    Surprised
+                  </label>
+                </div>
+              <button className="btn btn-default" type="submit">Save</button>
+              </form>
+
+            </div>
+          </div>
+        </div>
     );
   }
-}
-class SelectGender extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'both'};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('You want to draw: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Do you want to draw men or women?:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="both">both</option>
-            <option value="men">men</option>
-            <option value="women">women</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-class SelectSessions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'both'};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('What kind of session: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          What kind of session?:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="All the same length">All the same length</option>
-            <option value="Class mode">Class mode</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
+});
 
 var Session = React.createClass({
 
@@ -159,22 +151,22 @@ var Session = React.createClass({
 
   render: function () {
     return (
-      <div className="container">
+      <div className="Radio container">
         <div className="row">
           <div className="col-sm-12">
 
-          <br />
 
-          {this.state.text}
 
-          <input type="text" value={this.state.text} onChange={this.handleTextChange} />
+      {  /*  {this.state.text}
 
-          <br />
 
+          <input type="text" value={this.state.text} onChange={this.handleTextChange} /> */}
+
+
+        <h3 className="radioLabel">Female or male:</h3>
             <form onSubmit={this.handleFormSubmit}>
               <div className="radio">
-                <label>
-                  Do you want to draw men or women: <br/>
+                <label className="biglabel">
                   <input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange} />
                   Both
                 </label>
@@ -182,13 +174,13 @@ var Session = React.createClass({
               <div className="radio">
                 <label>
                   <input type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange}/>
-                  Men
+                  Female
                 </label>
               </div>
               <div className="radio">
                 <label>
                   <input type="radio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleOptionChange}/>
-                  Women
+                  Male
                 </label>
               </div>
               <button className="btn btn-default" type="submit">Save</button>
@@ -203,19 +195,54 @@ var Session = React.createClass({
 
 
 
+var NewButton = React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+  <img src={'LOGOEXAMPLE.png'} />
+  <button type="button" className="btn btn-default btn-lg button1">HOME</button>
+   <button type="button" className="btn btn-default btn-lg button2">ABOUT</button>
+  </div>
+  </div>
+  </div>
+  );
+}
+})
+
+var Picture= React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+
+         <br />
+      <img src="./pic1.jpg" className="img-responsive" alt="Responsive image" />
+      </div>
+      </div>
+      </div>
+    )
+  }
+})
+
 class App extends React.Component {
    render() {
       return (
-         <div className="centerDiv">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+            <NewButton />
             <Header/>
             <Content/>
-            <SelectGender/>
-            <SelectExpressions/>
-            <SelectSessions/>
+            <Expressions />
             <Session />
+            <Picture />
+         </div>
+         </div>
          </div>
       );
-   }
 }
-
+}
 export default App;
